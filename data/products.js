d4e4cb2ad557b957314488f7a45083ed697637bd
Nkhambhaliya <149261305+Nkhambhaliya@users.noreply.github.com>
2025-11-20 23:17:07 +0530

    improved ui

--- data/products.js
+++ data/products.js
@@ -1,69 +1,190 @@
 window.__SMG_PRODUCTS__ = [
+  
   {
-    id: 'shuttering-formwork',
-    name: 'Shuttering & Formwork',
-    description: 'Precision engineered shuttering solutions that provide dependable support for concrete structures of every scale.',
-    image: 'images/telescopic-spans.svg',
-    imageAlt: 'Telescopic span illustration',
+    id: 'adjustable_stirrup_head',
+    name: 'Adjustable Stirrup Head(U Jack)',
+    description: 'The Adjustable Stirrup Head is used at the top of the standard to provide an additional maximum adjustment of 300mm',
+    image: 'images/adjustable_stirrup_head.png',
+    imageAlt: 'Adjustable Stirrup Head(U Jack)',
     features: [
-      'Adjustable Telescopic Spans',
-      'MS Shuttering Plates',
-      'Formwork Systems',
-      'Column Clamps'
+      'U size 100x100x50 length 350mm',
+      'U size 100x100x50 length 450mm',
+    
     ]
   },
   {
-    id: 'cuplock-verticals',
-    name: 'Cuplock Verticals',
-    description: 'The foundation of our cuplock system, these verticals combine strength and speed with a proven cup and blade locking mechanism.',
-    image: 'images/cuplock-verticals.svg',
-    imageAlt: 'Cuplock verticals system'
-  },
-  {
-    id: 'horizontals-ledgers',
-    name: 'Horizontals / Ledgers',
-    description: 'Multiple ledger lengths that lock seamlessly into cuplock verticals to create a rigid, high-capacity scaffold structure.',
-    image: 'images/horizontals-ledgers.svg',
-    imageAlt: 'Horizontals and ledgers'
-  },
-  {
-    id: 'scaffolding-systems',
-    name: 'Scaffolding Systems',
-    description: 'Safe, efficient scaffolding packages with every component rated for demanding commercial and infrastructure projects.',
-    image: 'images/h-frame.svg',
-    imageAlt: 'Steel scaffolding components',
+    id: 'adjustable_base_jack',
+    name: 'Adjustable Base-Jack',
+    description: 'The adjustable base-jack is used at the bottom of the scaffold to provide an additional adjustment of up to 300mm',
+    image: 'images/adjustable_base_jack.png',
+    imageAlt: 'Adjustable Base-Jack',
     features: [
-      'Cuplock Verticals',
-      'Horizontals & Ledgers',
-      'H-Frames',
-      'Base Plates'
+      'Base plate 150x150 length 350mm',
+      'Base plate 150x150 length 450mm',
+    
     ]
   },
   {
-    id: 'accessories',
-    name: 'Accessories',
-    description: 'Complete range of scaffolding accessories built to withstand rugged site conditions and aggressive schedules.',
-    image: 'images/accessories.svg',
-    imageAlt: 'Scaffolding accessories',
+    id: 'cuplock_vertical_standard',
+    name: 'Cuplock Vertical/Standard',
+    description: 'Cup-lock standard have cup joints welded at every 500mm intervals, thus offering levels for working & also for bracing the standards while using in the slab support when loaded.',
+    image: 'images/cuplock_vertical_standard.png',
+    imageAlt: 'cuplock vertical standard',
     features: [
-      'Forged Fixed Coupler',
-      'Hex Nut with Water Barrier',
-      'Anchor Nut',
-      'Tie Rod'
+      'Available in sizes:',
+      '3.0 Mtr',
+      '2.5 Mtr',
+    '2.0 Mtr', 
+    '1.5 Mtr',
+    '1.0 Mtr',
+    '0.5 Mtr',
+  
+  ]
+  },
+  
+  {
+    id: 'horizontal_ledger',
+    name: 'Horizontal/Ledger',
+    description: 'These ledgers have forged blade ends which fit in the cup joint on the standard. They can be used in variety of Ledger size combinations to suit required bay size & loading conditions.',
+    image: 'images/Horizontal_Ledgers.png',
+    imageAlt: 'horizontal ledger',
+    features: [
+      'Available in sizes:',
+      '2.0 Mtr',
+      '1.5 Mtr',
+      '1.2 Mtr',
+      '0.9 Mtr',
     ]
   },
+
   {
-    id: 'hardware-components',
-    name: 'Hardware Components',
-    description: 'Premium nuts, bolts, rods, couplers, pins, and connectors that keep every shuttering or scaffolding setup secure.',
-    image: 'images/ms-shuttering-plate.svg',
-    imageAlt: 'Hardware components assortment',
+    id: 'adjustable_telescopic_prop',
+    name: 'Adjustable Telescopic Prop',
+    description: 'Prop is a labor and time saving device to support shutering and centering. It is made out of 60mm O.D & 48mm O.D. tubes moving in each other telescopically. A heavy duty S.G. iron nut provides fine adjustment of height of prop. The threads are rolled and not cut.',
+    image: 'images/adjustable_telescopic_prop.png',
+    imageAlt: 'adjustable telescopic prop',
     features: [
-      'Forged Swivel Coupler',
-      'Base Jack',
-      'Prop Nut',
-      'Shear Pin'
+      'Available in sizes:',
+      '2x2 Mtr',
+      '2x3 Mtr',
+      '3x3 Mtr',
     ]
-  }
+  },
+
+  {
+    id: 'ms_challi',
+    name: 'Ms Challi',
+    description: 'Ms challi for a make temporary platform with the help of cup-lock standards & ledger using for outer wall works like plaster, glass fitting etc.',
+    image: 'images/ms_challi.png',
+    imageAlt: 'ms challi',
+    
+  },
+
+  {
+    id: 'steel_channels',
+    name: 'Steel Channels',
+    image: 'images/steel_channels.png',
+    imageAlt: 'steel channels',
+    
+  },
+
+  {
+    id: 'prop_sleeve',
+    name: 'Prop Sleeve',
+    image: 'images/prop_sleeve.png',
+    imageAlt: 'Prop sleeve',
+  },
+
+  {
+    id: 'anchor_nut',
+    name: 'Anchor Nut',
+    image: 'images/anchor_nut.png',
+    imageAlt: 'anchor_nut',
+  },
+
+  {
+    id: 'h_d_anchor_nut',
+    name: 'H D Anchor Nut',
+    image: 'images/h_d_anchor_nut.png',
+    imageAlt: 'h_d_anchor_nut',
+  },
+
+  {
+    id: 'wing_nut',
+    name: 'Wing Nut',
+    image: 'images/wing_nut.png',
+    imageAlt: 'wing_nut',
+  },
+
+  {
+    id: 'tie_rod',
+    name: 'Tie Rod',
+    image: 'images/tie_rod.png',
+    imageAlt: 'tie_rod',
+  },
+
+  {
+    id: 't_toe_rod',
+    name: 'T Toe Rod',
+    image: 'images/t_toe_rod.png',
+    imageAlt: 't_toe_rod',
+  },
+
+  {
+    id: 'pressed_coupler_swivel',
+    name: 'Pressed Coupler Swivel',
+    image: 'images/pressed_coupler_swivel.png',
+    imageAlt: 'pressed_coupler_swivel',
+  },
+
+  {
+    id: 'ledger_blade',
+    name: 'Ledger Blade',
+    image: 'images/ledger_blade.png',
+    imageAlt: 'ledger_blade',
+  },
+
+  {
+    id: 'pressed_coupler_fixed',
+    name: 'Pressed Coupler Fixed',
+    image: 'images/pressed_coupler_fixed.png',
+    imageAlt: 'pressed_coupler_fixed',
+  },
+  
+  {
+    id: 'spigot_joint_pin',
+    name: 'Spigot (Joint Pin)',
+    image: 'images/spigot_joint_pin.png',
+    imageAlt: 'spigot_joint_pin',
+  },
+
+  {
+    id: 'wedge_pin',
+    name: 'Wedge Pin',
+    image: 'images/wedge_pin.png',
+    imageAlt: 'wedge_pin',
+  },
+
+  {
+    id: 'bottum_cut',
+    name: 'Bottum Cut',
+    image: 'images/bottum_cut.png',
+    imageAlt: 'bottum_cut',
+  },
+
+  {
+    id: 'u_plate',
+    name: 'U Plate',
+    image: 'images/u_plate.png',
+    imageAlt: 'u_plate',
+  },
+
+  {
+    id: 'base_plate',
+    name: 'Base Plate',
+    image: 'images/base_plate.png',
+    imageAlt: 'base_plate',
+  },
+
 ];
 
